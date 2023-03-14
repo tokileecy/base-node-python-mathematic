72160af3bfb9734e3acf98c6c35051d3f9ec29be
tokileecy <tokileecy@gmail.com>
2023-03-14 11:56:31 +0000

    init commit

---
+++ docker-entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+set -ea
+
+echo "Node modules not installed. Installing..."
+
+if [ -f "package.json" ]; then
+
+  if [ -f "yarn.lock" ]; then
+    
+    yarn install
+
+  else
+
+    npm install
+
+  fi
+
+fi
+
+echo "Starting App..."
+
+exec "$@"
